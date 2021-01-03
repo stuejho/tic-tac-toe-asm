ab4b4114fb54f3e1af1e6666522ab1f0581ccb1d
Ethan Ho <ethan.j.ho-1@ou.edu>
2021-01-03 21:00:36 +0000

    Outline program functions

--- main.asm
+++ main.asm
@@ -5,6 +5,25 @@
             section     .text
             global      _start
 _start:
+            ; call main game loop
+            call run_game
+
+            ; exit game
             mov         eax, SYS_EXIT
             mov         ebx, $0
             int         LINUX_SYSCALL
+
+run_game:
+            call print_board
+            call get_input
+            call process_input
+            ret
+
+print_board:
+            ret
+
+get_input:
+            ret
+
+process_input:
+            ret
